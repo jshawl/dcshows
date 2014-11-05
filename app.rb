3148c59f2d4e855926192cadcacdf2e45b6f36b0
Jesse Shawl <jesse@jshawl.com>
2014-11-05 17:23:09 +0000

    broken; gotta write my own xml

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class DCShows < Sinatra::Base
   get '/feed.xml' do
     url = "http://api.songkick.com/api/3.0/metro_areas/1409/calendar.xml?apikey=" + ENV['API_KEY']
     res = HTTParty.get( url )
-    @shows =  REXML::Document.new(res.body).root
+    @shows =  REXML::Document.new(res.body).root.elements[1].elements.to_a.join('')
     erb :feed
   end
 
