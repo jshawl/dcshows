8370df7523eda28954e31e39b98ce1a8b1f2a114
jshawl <jesse@jshawl.com>
2014-11-05 09:04:35 -0800

    format xml

--- app.rb
+++ app.rb
@@ -1,13 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
+require 'rexml/document'
 
 class DCShows < Sinatra::Base
 
   get '/feed.xml' do
     url = "http://api.songkick.com/api/3.0/metro_areas/1409/calendar.xml?apikey=" + ENV['API_KEY']
     res = HTTParty.get( url )
-    @shows =  res.body
+    @shows =  REXML::Document.new(res.body).root
     erb :feed
   end
 
